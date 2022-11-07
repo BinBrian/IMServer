fab70349ce892a5990f6ff161f6edec2e4cebb46
BinBrian <1246507584@qq.com>
2022-11-07 00:48:13 -0800

    Improve readability of project management

--- Service/build/cmake_install.cmake
+++ Service/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/ik/桌面/Service
+# Install script for directory: /home/brian/WorkSpace/IMServer/Service
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
+    set(CMAKE_INSTALL_CONFIG_NAME "")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -37,9 +37,18 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
+# Set default install directory permissions.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "/usr/bin/objdump")
+endif()
+
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("/home/ik/桌面/Service/build/src/cmake_install.cmake")
+  include("/home/brian/WorkSpace/IMServer/Service/build/model/cmake_install.cmake")
+  include("/home/brian/WorkSpace/IMServer/Service/build/redis/cmake_install.cmake")
+  include("/home/brian/WorkSpace/IMServer/Service/build/service/cmake_install.cmake")
+  include("/home/brian/WorkSpace/IMServer/Service/build/client/cmake_install.cmake")
+  include("/home/brian/WorkSpace/IMServer/Service/build/server/cmake_install.cmake")
 
 endif()
 
@@ -51,5 +60,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/ik/桌面/Service/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/brian/WorkSpace/IMServer/Service/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
